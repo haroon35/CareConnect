a7b3cb5a073ce661a2fd106c3581cbd14d572074
Haroon <you@example.com>
2025-01-25 03:14:56 +0500

    Sponsor an orphan component styled and designed.

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,13 @@ import "./globals.css";
 import Hero from "./components/Hero";
 import DonationRequests from "./components/DonationRequests";
 import OurVision from "./components/OurVision";
+import SponsorAnOrphan from "./components/SponsorAnOrphan";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <SponsorAnOrphan />
       <DonationRequests />
       <OurVision />
     </div>
